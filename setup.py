1000493126ee1ddb8f65c3db8247b61e22d511b5
junchen feng <frankfeng.pku@gmail.com>
2015-02-09 15:53:45 +0800

    reorganize for pipy publish

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyirt',
+  packages = ['pyirt'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python implementation of EM IRT, specializing in sparse massive data',
+  author = 'Junchen Feng',
+  author_email = 'frankfeng.pku@gmail.com',
+  url = 'https://github.com/junchenfeng/pyirt', # use the URL to the github repo
+  download_url = 'https://github.com/junchenfeng/pyirt/tarball/0.1', # I'll explain this in a second
+  keywords = ['IRT', 'EM algorithm'], # arbitrary keywords
+  classifiers = [],
+)
