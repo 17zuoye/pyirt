c8f8c8e0c722b223e5900d932ac7fdba5934b5d8
junchenfeng <frankfeng.pku@gmail.com>
2018-05-09 15:43:46 +0800

    go around pypi version restriction

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyirt',
-    version="0.3.3",
+    version="0.3.3.1",
     packages=['pyirt',
               'pyirt/solver',
               'pyirt/util', ],
@@ -12,7 +12,7 @@ setup(
     author_email='frankfeng.pku@gmail.com',
     include_package_data=True,
     url='https://github.com/junchenfeng/pyirt',
-    download_url='https://github.com/17zuoye/pyirt/archive/v0.3.3.tar.gz',
+    download_url='https://github.com/17zuoye/pyirt/archive/v0.3.3.1.tar.gz',
     keywords=['IRT', 'EM', 'big data'],
     zip_safe=False,
     platforms='any',
