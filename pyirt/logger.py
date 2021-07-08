23d9b9c5c0a80b8bbf7cc2c35d7feebff4d7ba68
masayuki.hirota <masayuki.hirota@classi.jp>
2021-07-08 13:09:55 +0900

    avoid duplicate logging

--- pyirt/logger.py
+++ pyirt/logger.py
@@ -6,27 +6,41 @@ import os
 class Logger():
     @staticmethod
     def logger(log_path):
+        logger = logging.getLogger()
+
+        # initialize logger
+        for h in logger.handlers[:]:
+            logger.removeHandler(h)
+            h.close()
+        logger.setLevel(logging.DEBUG)
+
+        # output log to console
+        console = logging.StreamHandler()
+        console.setLevel(logging.DEBUG)
+        console.setFormatter(
+            logging.Formatter(
+                fmt='%(asctime)s %(levelname)s: %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S'
+            )
+        )
+        logger.addHandler(console)
+
+        # output log to file
         if log_path is not None:
             log_dir = os.path.dirname(log_path)
             if not os.path.exists(log_dir):
                 os.makedirs(log_dir)
-            logging.basicConfig(
-                level=logging.DEBUG,
-                format='%(asctime)s %(pathname)s[line:%(lineno)d] %(levelname)s: %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S',
+            file_handler = logging.FileHandler(
                 filename=log_path,
-                filemode='w')
-        else:
-            logging.basicConfig(
-                level=logging.DEBUG,
-                format='%(asctime)s %(levelname)s: %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S')
+                mode='w'
+            )
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(
+                logging.Formatter(
+                    fmt='%(asctime)s %(pathname)s[line:%(lineno)d] %(levelname)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                )
+            )
+            logger.addHandler(file_handler)
 
-        # 创建一个handler，用于输出到控制台
-        console = logging.StreamHandler()
-        console.setLevel(logging.DEBUG)
-
-        logger = logging.getLogger()
-        # 给logger添加handler
-        logger.addHandler(console)
         return logger
